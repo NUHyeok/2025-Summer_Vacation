74aec6762069ac1788d2f6208b7c0607e39b1e30
나우혁 <nawooh119@gmail.com>
2025-08-07 14:34:45 +0900

    클래스 변수 학습

---
+++ user4.py
@@ -0,0 +1,33 @@
+# 클래스 변수 -> 모든 인스턴스 변수들이 공통으로 가지는 속성
+# 즉, 유저의 명수?
+
+class User:
+    
+    count = 0
+    
+    def __init__(self, name, email, passward): # 인스턴스가 생성되는 상황에서 자동으로 호출
+        self.name = name
+        self.email = email
+        self.passward = passward
+        User.count += 1
+        
+    def say_hello(self): # 행동을 정의하는 함수를 인스턴스 메소드 or 메소드
+        print(f"안녕하세요! 저는 {self.name}입니다!")
+    
+    def login(self, email, passward):
+        if self.email == email and self.passward == passward:
+            print("로그인 성공, 환영합니다.")
+            self.say_hello()
+        else:
+            print("로그인 실패, 없는 아이디거나 잘못된 비밀번호입니다.")
+            
+    def __str__(self):
+        return f"사용자 : {self.name}, 이메일 : {self.email}"
+
+user1 = User("강영훈", "younghoon@codeit.xyz", "12345")
+user2 = User("이윤수", "yoonsoo@codeit.xyz", "abcde") # 클래스로 부터 파생된 객체를 인스턴스라고 부름. 
+
+print(User.count)
+
+
+
